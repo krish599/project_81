059c84662afe19788593bcb75333f7abf91bf806
krish599 <76733539+krish599@users.noreply.github.com>
2021-05-09 07:54:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getElementById("2d");
+
+color = "red";
+
+ctx.beginPath;
+ctx.strokeStyle = color;
+ctx.lineWidth = 2;
+ctx.rect(150,143,430,200);
+ctx.stroke();
+
+    ctx.beginPath;
+    ctx.strokeStyle = color;
+    ctx.lineWidth = 2;
+    ctx.arc(250,200,40,0 , 2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath;
+    ctx.strokeStyle = black;
+    ctx.lineWidth = 3;
+    ctx.arc(250,200,40,0 , 2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath;
+    ctx.strokeStyle = red;
+    ctx.lineWidth = 4;
+    ctx.arc(250,200,40,0 , 2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath;
+    ctx.strokeStyle = yellow;
+    ctx.lineWidth = 4;
+    ctx.arc(250,200,40,0 , 2*Math.PI);
+    ctx.stroke();
+
+    ctx.beginPath;
+    ctx.strokeStyle = green;
+    ctx.lineWidth = 5;
+    ctx.arc(250,200,40,0 , 2*Math.PI);
+    ctx.stroke();
+
+
